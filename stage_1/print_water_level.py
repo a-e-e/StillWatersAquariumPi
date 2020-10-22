ef5dffd64fc630a3817ef56f2a2100a19dcedd92
simonjung <sjung@dendisoftware.com>
2020-10-21 22:39:30 -0400

    clean up

--- stage_1/print_water_level.py
+++ stage_1/print_water_level.py
@@ -5,18 +5,6 @@ import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D22)
-
-# create the mp object
-mcp = MCP.MCP3008(spi, cs)
-
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
-
 
 def get_water_level_inches(voltage):
     """
@@ -26,12 +14,34 @@ def get_water_level_inches(voltage):
     :param voltage:
     :return:
     """
-    water_level_inches = (voltage - 2.49) / -0.0978
-    return water_level_inches
+    slope = -0.0978
+    y_intercept = 2.49
+    water_level_inches = (voltage - y_intercept) / slope
+    return round(water_level_inches, 2)
 
 
-while True:
-    print('Raw ADC Value: ', chan.value)
-    print('ADC Voltage: ' + str(chan.voltage) + 'V')
-    print('Water Level: ' + str(get_water_level_inches(chan.voltage)))
-    time.sleep(.5)
+def main():
+    """
+    Print the water level every .5 seconds
+    :return:
+    """
+    # create the spi bus
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+    # create the cs (chip select)
+    cs = digitalio.DigitalInOut(board.D22)
+
+    # create the mcp object
+    mcp = MCP.MCP3008(spi, cs)
+
+    # create an analog input channel on pin 0
+    water_level_sensor = AnalogIn(mcp, MCP.P0)
+
+    while True:
+        print('ADC Voltage: ' + str(round(water_level_sensor.voltage, 2)) + 'V')
+        print('Water Level: ' + str(get_water_level_inches(water_level_sensor.voltage)) + ' inches')
+        time.sleep(.5)
+
+
+if __name__ == '__main__':
+    main()
